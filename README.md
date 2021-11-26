7f68daf869fc1ecf478c65a92da32b3d905f2277
shleecloud <shleecloud@naver.com>
2021-11-26 10:20:20 +0900

    Init: directory 생성

---
+++ README.md
@@ -0,0 +1,3 @@
+# LearnRegex!
+
+<img width="759" alt="learnregex-logo" src="https://user-images.githubusercontent.com/62797565/143479379-106673e5-05e7-4447-9138-979457152e54.png">
---
+++ scripts/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /home/ubuntu/LearnRegex/server
+
+export DATABASE_USERNAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USERNAME --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_ADDR=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_ADDR --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
+export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
+
+authbind --deep pm2 start index.js
