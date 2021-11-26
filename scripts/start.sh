d01bacd458e134eae21a1236fa6652b450167a79
hyucki <gurl97@naver.com>
2021-11-26 11:47:54 +0900

    Feat: http 서버 구축

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,4 +8,4 @@ export DATABASE_ADDR=$(aws ssm get-parameters --region ap-northeast-2 --names DA
 export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
 
-authbind --deep pm2 start index.js
+authbind --deep pm2 start app.js
---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
+  })
+);
+
+app.listen(4000, () => console.log('📚📚📚📚 LearnRegex server started ☁️  http://localhost:4000'));
