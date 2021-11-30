06967c29c5a3969b9e8eb051c87059fc07cdcbd2
EUNYUSEO <eyseo328@gmail.com>
2021-11-30 14:44:48 +0900

    Feat: QuizListElement 구성 #40
    QuizList에서 보여줄 QuizListElement 구성

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,16 @@
+import React from 'react';
+
+// 확인을 위해 넣어둠 - 삭제해도 됨
+// import QuizListElement from './components/QuizListElement';
+
 function App() {
-  return <div>HelloWorld</div>;
+  return (
+    <div>
+      HelloWorld
+      {/* 확인을 위해 넣어둠 - 삭제해도 됨
+      <QuizListElement id={1} title={'비밀번호 유효성 검사'} nickname={'김코딩'} count={3} isClear={true} /> */}
+    </div>
+  );
 }
 
 export default App;
