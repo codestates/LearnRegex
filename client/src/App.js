1a1e85cb2067a8f8d7768a7c7c2f4476d747d6b6
yunjink <chloeeekim1109@gmail.com>
2021-11-30 18:04:23 +0900

    Feat : 네비게이션 구성

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+// import { Routes, Route } from 'react-router-dom';
+import Navigation from './components/navigation/Navigation';
+
 function App() {
-  return <div>HelloWorld</div>;
+  return (
+    <>
+      <Navigation />
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/components/navigation/MenuItems.js
@@ -0,0 +1,12 @@
+export const MenuItems = [
+  {
+    title: '내 정보',
+    path: '/myinfo',
+    cName: 'dropdown-link',
+  },
+  {
+    title: '로그아웃',
+    path: '/signout',
+    cName: 'dropdown-link',
+  },
+];
