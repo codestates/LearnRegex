abdc22cb1364e8b92559583f095692cc6216a4a5
EUNYUSEO <eyseo328@gmail.com>
2021-11-30 21:43:39 +0900

    Feat: SolveQuiz 페이지 레이아웃 구현
    SolveQuiz 페이지 기본적인 레이아웃 구현
    - 사용자 본인이 제출한 문제면 수정 / 삭제 text 보이고 아닐시 수정 / 삭제 text 안보임
    - 모두 dummyData로 구성함, 이후 서버 구현되면 dummyData -> data로 수정하면 됨
    - 뒤로가기 아이콘 생성

--- client/src/pages/SolveQuiz.js
+++ client/src/pages/SolveQuiz.js
@@ -14,13 +14,40 @@ function SolveQuiz() {
     dispatch(getQuiz(id));
   }, [id, dispatch]);
 
+  const dummyData = {
+    id: 1,
+    title: '제목1',
+    nickname: '김코딩',
+    testCase:
+      'I don’t want a lot for Christmas There’s just one thing I need I don’t care about the presents Underneath the Christmas tree I just want you for my own More than you could ever know Make my wish come true... shleecloud@gmail.com All I want for Christmas Is you... I don’t want a lot for Christmas There is just one thing I need And I don’t care about presents Underneath the Christmas tree I don’t need to hang my stocking There upon the fireplace Santa Claus won’t make me happy With a toy on Christmas day I just want you for my own More than you could ever know Make my wish come true All I want for Christmas is you...',
+    testCaseTarget: 'shleecloud@gmail.com',
+    answer: '/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i',
+    isMade: true,
+  };
+
   return (
     <>
       <div>
-        <ImArrowLeft2 size="50" />
-        <QuizForm />
-        {/* TODO: 서버 구현되면 아래 주석 풀기 */}
-        {/* <QuizForm testCase={data.tesCase} testCaseTarget={data.testCaseTarget} answer={data.answer} page={"solve"} /> */}
+        <div>
+          <ImArrowLeft2 size="50" />
+        </div>
+        <div>
+          <div>
+            <div>제목: {dummyData.title}</div>
+            <div>닉네임: {dummyData.nickname}</div>
+            {dummyData.isMade ? (
+              <div>
+                <span>수정</span>
+                <span>삭제</span>
+              </div>
+            ) : (
+              ''
+            )}
+          </div>
+          <div>
+            <QuizForm testCase={dummyData.testCase} testCaseTarget={dummyData.testCaseTarget} answer={dummyData.answer} page={'solve'} />
+          </div>
+        </div>
       </div>
     </>
   );
