26f05ab6b43ba1be9fcd0da33c8fceb9df2b0696
hyucki <gurl97@naver.com>
2021-11-30 17:48:59 +0900

    Feat: 추상화 함수 생성 (#59, #60, #121) 토큰, 로그인 인증, 유효성 및 중복 검사 함수 생성

---
+++ server/controllers/inspectfunction/index.js
@@ -0,0 +1,54 @@
+const { users } = require('../../models');
+
+module.exports = {
+  validation: ({ email, nickname, password }) => {
+    const result = {
+      isValidEmail: true,
+      isValidNickname: true,
+      isValidPassword: true,
+    };
+
+    if (email) {
+      if (!/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/g.test(email)) {
+        result.isValidEmail = false;
+      }
+    }
+
+    if (nickname) {
+      if (!/^[가-힣a-zA-Z0-9]{2,10}$/g.test(nickname)) {
+        result.isValidNickname = false;
+      }
+    }
+
+    if (password) {
+      if (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/g.test(password)) {
+        result.isValidPassword = false;
+      }
+    }
+    return result;
+  },
+  confliction: async ({ email, nickname }) => {
+    // 들어온게 있다면 실행하도록
+    const result = {
+      isConflictEmail: true,
+      isConflictNickname: true,
+    };
+
+    let isConflict;
+
+    if (email) {
+      isConflict = await users.findOne({ where: { email } });
+      if (isConflict) {
+        result.isConflictEmail = false;
+      }
+    }
+
+    if (nickname) {
+      isConflict = await users.findOne({ where: { nickname } });
+      if (isConflict) {
+        result.isConflictNickname = false;
+      }
+    }
+    return result;
+  },
+};
--- server/router/myinfo.js
+++ server/router/myinfo.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { isAuth } = require('../controllers/authfunction');
 
-const myinfo = require('../contrellers/myinfo');
+const myinfo = require('../controllers/myinfo');
 
-router.get('/', myinfo.getmyinfo);
-router.patch('/', myinfo.editmyinfo);
-router.delete('/', myinfo.deletemyinfo);
+router.get('/', isAuth, myinfo.getmyinfo);
+router.patch('/', isAuth, myinfo.editmyinfo);
+router.delete('/', isAuth, myinfo.deletemyinfo);
 
 module.exports = router;
