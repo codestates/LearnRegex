587d81ceb5dd53ef30f04d3dffd5b00a68226445
EUNYUSEO <eyseo328@gmail.com>
2021-11-30 23:30:59 +0900

    Feat: InputQuiz 컴포넌트 상태 생성
    사용자가 입력할 수 있는 곳에서 입력한다면 해당 key에 맞게 상태 변화됨

--- client/src/components/InputQuiz.js
+++ client/src/components/InputQuiz.js
@@ -1,39 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ImArrowLeft2 } from 'react-icons/im';
 
 function InputQuiz({ page = 'make' }) {
+  const [content, setContent] = useState({ title: '', testCase: '', testCaseTarget: '', answer: '', explanation: '' });
+
+  const handleInputValue = (key) => (e) => {
+    setContent({ ...content, [key]: e.target.value });
+  };
+
   return (
     <>
       <div>
         <div>
           <ImArrowLeft2 size="50" />
         </div>
-        <div>
+        <form onSubmit={(e) => e.preventDefault()}>
           <div>
-            <input type="text" placeholder="제목을 입력하세요" />
+            <input type="text" value={content.title} placeholder="제목을 입력하세요" onChange={handleInputValue('title')} />
           </div>
           <div>
             <h2>Test Case</h2>
             <div>
-              <textarea placeholder="testCase를 입력하세요" />
+              <textarea value={content.testCase} placeholder="testCase를 입력하세요" onChange={handleInputValue('testCase')} />
             </div>
             <div>
-              <textarea placeholder="testCaseTarget을 입력하세요" />
+              <textarea value={content.testCaseTarget} placeholder="testCaseTarget을 입력하세요" onChange={handleInputValue('testCaseTarget')} />
             </div>
           </div>
           <div>
             <h2>정답 / 해설</h2>
             <div>
-              <input type="text" placeholder="정답을 쓰세요" />
+              <input type="text" value={content.answer} placeholder="정답을 쓰세요" onChange={handleInputValue('answer')} />
             </div>
             <div>
-              <textarea type="text" placeholder="해설을 쓰세요" />
+              <textarea type="text" value={content.explanation} placeholder="해설을 쓰세요" onChange={handleInputValue('explanation')} />
             </div>
           </div>
           <div>
-            <button>{page === 'make' ? '퀴즈 업로드' : '수정 완료'}</button>
+            <button type="submit">{page === 'make' ? '퀴즈 업로드' : '수정 완료'}</button>
           </div>
-        </div>
+        </form>
       </div>
     </>
   );
