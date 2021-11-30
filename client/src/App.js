94817bf4699c83cad91ddc35e5e6ee1f2bc97243
EUNYUSEO <eyseo328@gmail.com>
2021-11-30 17:51:19 +0900

    Feat: QuizForm 구성
    QuizForm 레이아웃 구성

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 // import QuizList from './pages/QuizList'; // 확인을 위해 넣어둔 코드임! 삭제해도 괜찮음
+import QuizForm from './components/QuizForm';
 
 function App() {
   return (
     <div>
       HelloWorld
       {/* <QuizList /> // 확인을 위해서 넣어둔 코드임! 삭제해도 괜찮음 */}
+      <QuizForm />
     </div>
   );
 }
