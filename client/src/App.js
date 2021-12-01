e67743c88b34b4935d5976344d40043ad54b894c
EUNYUSEO <eyseo328@gmail.com>
2021-12-01 23:14:37 +0900

    Feat: QuizForm 정답 입력시 LocalStorage 저장

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import SignUp from './components/user/SignUp';
-import EditUserInfo from './pages/myinfo/EditUserInfo';
-import EditUserPassword from './pages/myinfo/EditUserPassword';
-import GetMyInfo from './pages/myinfo/GetMyInfo';
-import QuizForm from './components/QuizForm';
 import './App.css';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import SignIn from './components/userinfo/SignIn';
-import QuizList from './pages/QuizList';
 import SolveQuiz from './pages/SolveQuiz';
-import EditQuiz from './pages/EditQuiz';
-import MakeQuiz from './pages/MakeQuiz';
 import dotenv from 'dotenv';
 dotenv.config();
 
 function App() {
   return (
-    <Routes>
-      {/* <Route path="/" element={<Navbar />} /> */}
-      <Route path="/" element={<Sidebar />} />
-    </Routes>
+    <>
+      <SolveQuiz />
+    </>
   );
 }
 
