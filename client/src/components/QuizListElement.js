0792252b4f768318a2450b44d0310411ded75ab6
EUNYUSEO <eyseo328@gmail.com>
2021-12-02 20:34:29 +0900

    Update: QuizList에서 서버로부터 응답 받은 데이터 보여주기 성공

--- client/src/components/QuizListElement.js
+++ client/src/components/QuizListElement.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function QuizListElement({ id, title, nickname, count, isClear }) {
+function QuizListElement({ data }) {
   // TODO: isClear의 값에 따라 button props 다르게 전달하기 -> 도전하기 색상 변경
   // TODO: button 클릭 시 QuizSolve 페이지로 이동 -> id props로 전달
 
@@ -9,13 +9,13 @@ function QuizListElement({ id, title, nickname, count, isClear }) {
     <>
       <div className="container">
         <div>
-          <div>제목: {title}</div>
-          <div>출제자: {nickname}</div>
-          <div>이 퀴즈를 {count} 명이 풀었습니다!</div>
+          <div>제목: {data.title}</div>
+          <div>출제자: {data.nickname}</div>
+          <div>이 퀴즈를 {data.count} 명이 풀었습니다!</div>
         </div>
         <div>
           <button>
-            <Link to={`/quiz/${id}`}>도전하기</Link>
+            <Link to={`/quiz/${data.id}`}>도전하기</Link>
           </button>
         </div>
       </div>
--- client/src/pages/quiz/QuizList.js
+++ client/src/pages/quiz/QuizList.js
@@ -7,30 +7,22 @@ import { BsPencilSquare } from 'react-icons/bs';
 
 function QuizList() {
   const { data } = useSelector((state) => state.quizzes.list);
-  // console.log(data);
+  console.log(data);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getQuizzes());
   }, [dispatch]);
 
-  // DummyData = 받아온 data
-  const dummyData = [
-    { id: 1, title: '제목1', nickname: '일삼이', count: 1, isClear: true },
-    { id: 2, title: '제목2', nickname: '이삼이', count: 2, isClear: false },
-    { id: 3, title: '제목3', nickname: '삼삼이', count: 3, isClear: false },
-    { id: 4, title: '제목4', nickname: '사삼이', count: 4, isClear: false },
-  ];
+  if (!data) return <div>loading....</div>;
 
   return (
     <>
       <Link to="/makequiz">
         <BsPencilSquare size="50" />
       </Link>
-      {/* TODO: QuizListElement에게 data의 id, title, nickname, count, isClear 전달하기 
-      <QuizListElement id={1} title={'제목이당'} nickname={'김코딩'} count={3} isClear={true} /> */}
-      {dummyData.map((el) => (
-        <QuizListElement key={el.id} id={el.id} title={el.title} nickname={el.nickname} count={el.count} isClear={el.isClear} />
+      {data.map((el) => (
+        <QuizListElement key={el.id} data={data} />
       ))}
     </>
   );
