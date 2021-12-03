38a5594dffdfb25ae270b171b3d8fdbf7fe508cc
EUNYUSEO <eyseo328@gmail.com>
2021-12-03 16:49:12 +0900

    Update: EditQuiz에서 퀴스 수정 성공(PATCH 요청)

--- client/src/lib/submitQuiz.js
+++ client/src/lib/submitQuiz.js
@@ -2,20 +2,24 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 export function submitQuiz(data, content) {
+  // console.log(content);
+  // console.log(data);
   const isValid = (content) => content.title !== '' && content.testCase !== '' && content.testCaseTarget !== '' && content.answer !== '' && content.explanation !== '';
   // console.log(isValid(content));
-  // console.log(content);
-
   if (!isValid(content)) return alert('모든 항목에 입력하세요!!'); // TODO: 한 칸이라도 비워져있으면 빨간 테두리 + 살짝 진동 애니메이션
 
   if (!!data) {
-    // TODO: PATCH 요청 보내기 - http://localhost:4000/quiz/edit
-    console.log('----- PATCH 요청 -----');
-    console.log(content);
-    console.log('----- PATCH 요청 -----');
-    alert('퀴즈 수정 되었습니다!');
+    axios
+      .patch(`http://localhost:4000/quiz?quizId=${data.id}`, content)
+      .then((res) => {
+        console.log('patch 요청 성공');
+        alert('퀴즈 수정 되었습니다!');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     // ! 해당 퀴즈로 다시 돌아가면서 새로고침됨
-    // document.location.href = `/quiz/${data.id}`;
+    document.location.href = `/quiz/${data.id}`;
   } else {
     axios
       .post(`http://localhost:4000/quiz`, content)
