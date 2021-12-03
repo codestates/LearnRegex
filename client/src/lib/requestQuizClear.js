309659129cd1840ac680ca33636f0200bc51f6fd
EUNYUSEO <eyseo328@gmail.com>
2021-12-03 22:42:22 +0900

    [WIP]Update: 체크 이모지 떴을 때 서버 요청
    서버 요청-응답은 성공했으나 비회원일 때 / 회원이지만 문제를 풀었을 때 / 회원이고 문제를 풀지 않았을 때를 나눠야함

--- client/src/lib/requestQuizClear.js
+++ client/src/lib/requestQuizClear.js
@@ -3,12 +3,10 @@ axios.defaults['withCredentials'] = true;
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 const requestQuizClear = async (quizId) => {
+  console.log(quizId);
   if (!quizId) return;
   try {
-    const result = await axios.post(
-      `${process.env.REACT_APP_SERVER_ADDR}/quiz/clear`, //
-      { quizId }
-    );
+    const result = await axios.post(`${process.env.REACT_APP_SERVER_ADDR}/quiz/clear?quizId=${quizId}`);
     console.log(result);
     return true;
   } catch (error) {
