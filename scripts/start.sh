b981f1b876a287dda1678c9319f6cef7c5e3408b
hyucki <gurl97@naver.com>
2021-12-06 17:11:55 +0900

    데이터베이스 연결

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,5 +7,8 @@ export DATABASE_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DA
 export DATABASE_ADDR=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_ADDR --query Parameters[0].Value | sed 's/"//g')
 export DATABASE_PORT=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PORT --query Parameters[0].Value | sed 's/"//g')
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
+export GOOGLE_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_CLIENT_ID --query Parameters[0].Value | sed 's/"//g')
+export GOOGLE_CLIENT_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_CLIENT_SECRET --query Parameters[0].Value | sed 's/"//g')
+export GOOGLE_REDIRECT_URI=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_REDIRECT_URI --query Parameters[0].Value | sed 's/"//g')
 
 authbind --deep pm2 start app.js
