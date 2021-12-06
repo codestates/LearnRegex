0411057f4529d499bb3a06272e2d7949f8d488de
shleecloud <shleecloud@naver.com>
2021-12-06 22:25:27 +0900

    Feat: 실시간 CSS 변화, 1회 검색

--- client/src/components/QuizForm.js
+++ client/src/components/QuizForm.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Interweave from 'interweave';
+import style from './devQuizFormStyle.css';
 import { saveAnswer } from '../modules/answer';
 import requestQuizClear from '../lib/requestQuizClear';
 
@@ -19,49 +21,38 @@ function QuizForm({ data, orderPage }) {
   };
 
   // * 정규표현식 분류
-  let highlightedTestCase = '<pre>{`';
+  // let highlightedTestCase = '<pre>{`';
+  let highlightedTestCase = '';
   const getRegExp = (testCase) => {
-    // 잘못된 정규표현식이 들어오면 빈 문자열 반환
+    // * 정규표현식 만들기
+    let myRegex = '';
     try {
-      // flag와 pattern 분리
-      // replace 함수 두 번째 인자로 '$1' 값을 부여해서 소괄호로 묶인 첫 번째 그룹을 가져올 수 있다.
-      // const flags = inputRegex.replace(/.*\/([gimy]*)$/, '$1');
       const flags = 'g';
-      // replace 함수 안에서 flags 변수를 합쳐서 정규표현식을 만듦
-      // 방금 만든 정규표현식으로 inputRegex 내부 패턴값을 소괄호로 묶어서 추출
-      // const pattern = inputRegex.replace(new RegExp('^/(.*?)/' + flags + '$'), '$1');
-      const pattern = inputRegex;
-      const myRegex = new RegExp(pattern, flags);
-      console.log(myRegex);
-
-      // * Realtime CSS
-      let matchArray;
-      let startIndex = 0;
-      let lastIndex = 0;
-      while ((matchArray = myRegex.exec(testCase)) !== null) {
-        lastIndex = matchArray.index;
-        highlightedTestCase += data.testCase.substring(startIndex, lastIndex);
-        highlightedTestCase += "<span class='found'>" + matchArray[0] + '</span>';
-        startIndex = myRegex.lastIndex;
-        console.log(matchArray);
-      }
-      console.log('shleedev', matchArray);
-      highlightedTestCase += data.testCase.substring(startIndex, data.testCase.length);
-      highlightedTestCase += '`}</pre>';
-      // * Just Print
-      // const myRegexExec = myRegex.exec(testCase);
-      // console.log(myRegexExec);
-      // highlightedTestCase = myRegexExec;
+      const pattern = inputRegex || '^$';
+      myRegex = new RegExp(pattern, flags);
     } catch (e) {
       console.log(e);
-      return '';
+      myRegex = new RegExp('^$', 'g');
     }
+    console.log('myRegex:', myRegex);
+
+    // * Realtime CSS
+    let matchArray;
+    let startIndex = 0;
+    let lastIndex = 0;
+    let result = '';
+    if ((matchArray = myRegex.exec(testCase)) !== null) {
+      lastIndex = matchArray.index;
+      highlightedTestCase += data.testCase.substring(startIndex, lastIndex);
+      highlightedTestCase += "<span class='found'>" + matchArray[0] + '</span>';
+      startIndex = myRegex.lastIndex;
+      result = matchArray[0];
+    }
+    highlightedTestCase += data.testCase.substring(startIndex, data.testCase.length);
+    return result;
   };
   // * 정규표현식 적용 결과
   let regExpResult = getRegExp(data.testCase);
-  if (Array.isArray(regExpResult)) {
-    regExpResult = regExpResult[0];
-  }
 
   let isCorrectReg = regExpResult === data.testCaseTarget;
   const timeWait = useRef();
@@ -73,27 +64,25 @@ function QuizForm({ data, orderPage }) {
     }, 2000);
   }, [inputRegex]);
 
-  const handleDevPatch = () => {
-    // console.log(myRegex.exec(testCase));
-  };
-
   return (
     <>
       <div>
         <div>
           <h2>Test Case</h2>
           <div>
-            <span>{highlightedTestCase}</span>
+            <Interweave content={highlightedTestCase} />
           </div>
           <h2>Test Case Target</h2>
           <div>
-            data.testCaseTarget
+            {data.testCaseTarget}
             <div>{isCorrectReg ? '✅' : '❌'}</div>
           </div>
         </div>
         <div>
           <h2>My Regexp</h2>
-          <div>{/* <span>{regExpResult}</span> */}</div>
+          <div>
+            <span>{regExpResult}</span>
+          </div>
           <div>
             <input type="text" value={inputRegex || ''} placeholder="정규표현식을 입력하세요!" onChange={handleAnswer} size="100" />
           </div>
