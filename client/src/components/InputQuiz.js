5f70e334f1e05eb6cb60b29e4f0b00773eaa005b
EUNYUSEO <eyseo328@gmail.com>
2021-12-07 23:54:22 +0900

    [WIP]Feat/ InputQuiz - checkEmpty #69

--- client/src/components/InputQuiz.js
+++ client/src/components/InputQuiz.js
@@ -2,15 +2,32 @@ import React, { useState, useEffect } from 'react';
 import BackButton from '../components/BackButton';
 import { submitQuiz } from '../lib/requestQuiz';
 import { limitChar } from '../lib/limitChar';
+import styled from 'styled-components';
+
+export const Div = styled.div`
+  border: 1px solid ${({ isEmpty }) => (isEmpty ? 'red' : 'black')};
+`;
 
 function InputQuiz({ data }) {
   const [content, setContent] = useState({ title: '', testCase: '', testCaseTarget: '', answer: '', explanation: '' });
+  const isEmpty = false;
 
   const handleInputValue = (key) => (e) => {
-    // console.log(e.target.maxLength);
-    // console.log(e.target.value.length);
-    let result = e.target.maxLength > 0 ? limitChar(e.target.value.length, e.target.maxLength) : true;
+    let result = limitChar(e.target.value.length, e.target.maxLength);
     if (!!result) setContent({ ...content, [key]: e.target.value });
+  };
+
+  // * 빈 칸인지 확인
+  const handleSubmitQuiz = () => {
+    const contentKeys = Object.keys(content).slice(); // content의 key들만 담은 배열 = [title, testCase, testCaseTarget, answer, explanation]
+
+    const result = contentKeys.reduce((acc, cur) => {
+      // content[key]의 값이 빈 문자열인 것만 담은 배열
+      if (content[cur] === '') return acc.concat(cur);
+      else return acc;
+    }, []);
+    console.log(result);
+    // submitQuiz(data, content)
   };
 
   useEffect(() => {
@@ -27,29 +44,29 @@ function InputQuiz({ data }) {
           <BackButton />
         </div>
         <form onSubmit={(e) => e.preventDefault()}>
-          <div>
+          <Div isEmpty={isEmpty === true}>
             <input type="text" value={content.title} placeholder="제목을 입력하세요" maxLength="20" onChange={handleInputValue('title')} />
-          </div>
+          </Div>
           <div>
             <h2>Test Case</h2>
-            <div>
+            <Div isEmpty={isEmpty === false}>
               <textarea value={content.testCase} placeholder="testCase를 입력하세요" maxLength="400" onChange={handleInputValue('testCase')} />
-            </div>
-            <div>
-              <textarea value={content.testCaseTarget} placeholder="testCaseTarget을 입력하세요" onChange={handleInputValue('testCaseTarget')} />
-            </div>
+            </Div>
+            <Div isEmpty={isEmpty}>
+              <textarea value={content.testCaseTarget} placeholder="testCaseTarget을 입력하세요" maxLength="400" onChange={handleInputValue('testCaseTarget')} />
+            </Div>
           </div>
           <div>
             <h2>정답 / 해설</h2>
-            <div>
-              <input type="text" value={content.answer} placeholder="정답을 쓰세요" onChange={handleInputValue('answer')} />
-            </div>
-            <div>
+            <Div isEmpty={isEmpty}>
+              <input type="text" value={content.answer} placeholder="정답을 쓰세요" maxLength="100" onChange={handleInputValue('answer')} />
+            </Div>
+            <Div isEmpty={isEmpty}>
               <textarea type="text" value={content.explanation} placeholder="해설을 쓰세요" maxLength="400" onChange={handleInputValue('explanation')} />
-            </div>
+            </Div>
           </div>
           <div>
-            <button type="submit" onClick={() => submitQuiz(data, content)}>
+            <button type="submit" onClick={() => handleSubmitQuiz()}>
               {!!data ? '수정 완료' : '퀴즈 업로드'}
             </button>
           </div>
--- client/src/lib/requestQuiz.js
+++ client/src/lib/requestQuiz.js
@@ -19,14 +19,6 @@ export function deleteQuiz(id) {
 }
 
 export function submitQuiz(data, content) {
-  // console.log(content);
-  // console.log(data);
-
-  const isValid = (content) => content.title !== '' && content.testCase !== '' && content.testCaseTarget !== '' && content.answer !== '' && content.explanation !== '';
-  // console.log(isValid(content));
-
-  if (!isValid(content)) return alert('모든 항목에 입력하세요!!'); // TODO: 한 칸이라도 비워져있으면 빨간 테두리 + 살짝 진동 애니메이션
-
   // 퀴즈 수정 요청
   if (!!data) {
     axios
@@ -40,6 +32,7 @@ export function submitQuiz(data, content) {
         console.log(err);
       });
   }
+
   // 퀴즈 등록 요청
   else {
     axios
