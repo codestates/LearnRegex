b8b39930c0f2d9b1ca77676849c21bb08258e1dc
hyucki <gurl97@naver.com>
2021-12-07 22:11:27 +0900

    scripts.js 환경변수 불러오는 코드 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,5 +12,7 @@ export DOMAIN_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DOMA
 export GOOGLE_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_CLIENT_ID --query Parameters[0].Value | sed 's/"//g')
 export GOOGLE_CLIENT_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names GOOGLE_CLIENT_SECRET --query Parameters[0].Value | sed 's/"//g')
 export KAKAO_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names KAKAO_CLIENT_ID --query Parameters[0].Value | sed 's/"//g')
+export GITHUB_CLIENT_ID=$(aws ssm get-parameters --region ap-northeast-2 --names GITHUB_CLIENT_ID --query Parameters[0].Value | sed 's/"//g')
+export GITHUB_CLIENT_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names GITHUB_CLIENT_SECRET --query Parameters[0].Value | sed 's/"//g')
 
 authbind --deep pm2 start app.js
