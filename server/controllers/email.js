1ccaca0194f19b2289b0995d8fbf713f796464b0
hyucki <gurl97@naver.com>
2021-12-09 17:42:17 +0900

    Modify: 이메일 변경 시 링크 통해 접속 시 변경 가능하게 수정

--- server/controllers/email.js
+++ server/controllers/email.js
@@ -41,6 +41,45 @@ module.exports = {
     }
   },
 
+  // 이메일 변경
+  editEmail: async (req, res) => {
+    try {
+      const onceToken = req.body.token;
+
+      if (!onceToken) {
+        return res.status(400).send({ message: 'empty token' });
+      }
+      // 임시토큰을 검증한 뒤 해당하는 유저 정보 조회
+      const verifyToken = isVerify(onceToken);
+
+      if (!verifyToken) {
+        return res.status(406).send({ message: 'invalid token' });
+      }
+
+      const userInfo = await users.findOne({
+        where: { id: verifyToken.id },
+        attributes: ['id', 'email', 'nickname', 'socialType', 'verifyEmail'],
+        raw: true,
+      });
+
+      // 이미 이메일을 수정한 경우
+      if (userInfo.email === verifyToken.email) {
+        return res.header({ isLogin: false }).status(200).send({ message: 'already edit email' });
+      }
+
+      // 유저의 이메일을 수정해주고 수정한 이메일로 제대로 된 토큰 발급 하여 응답
+      await users.update({ email: verifyToken.email }, { where: { id: userInfo.id } });
+      userInfo.email = verifyToken.email;
+
+      const token = getToken(userInfo);
+
+      sendToken(res, token);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send({ message: 'server error' });
+    }
+  },
+
   // 비밀번호 재설정
   resetPassword: async (req, res) => {
     try {
