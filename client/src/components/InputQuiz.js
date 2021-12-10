7617bc5d8feade4310ca88373baf306148831106
shleecloud <shleecloud@naver.com>
2021-12-10 15:32:36 +0900

    Feat: 퀴즈 만들기 testCaseTarget 자동 입력

--- client/src/components/InputQuiz.js
+++ client/src/components/InputQuiz.js
@@ -20,11 +20,29 @@ function InputQuiz({ data }) {
   const [content, setContent] = useState({ title: '', testCase: '', testCaseTarget: '', answer: '', explanation: '' });
   const [isEmpty, setIsEmpty] = useState({ title: false, testCase: false, testCaseTarget: false, answer: false, explanation: false });
 
+  // * --------- 텍스트창 입력 ---------
   const handleInputValue = (key) => (e) => {
     if (isEmpty[key] === true) setIsEmpty({ ...isEmpty, [key]: false }); // 빈 칸이었다가 무언가 입력 시  빨간 테두리 사라짐
-
     let result = limitChar(e.target.value.length, e.target.maxLength);
-    if (!!result) setContent({ ...content, [key]: e.target.value });
+
+    // * 정규표현식 관련 변수 초기화
+    let testCase = content.testCase;
+    let answer = content.answer;
+    if (key === 'testCase') testCase = e.target.value;
+    else if (key === 'answer') answer = e.target.value;
+
+    // * 정규표현식 만들기
+    let myRegex = '';
+    try {
+      const flags = 'g';
+      const pattern = answer || '^$';
+      myRegex = new RegExp(pattern, flags);
+    } catch (e) {
+      myRegex = new RegExp('^$', 'g');
+    }
+    let matchArray = myRegex.exec(testCase);
+    let testCaseTarget = Array.isArray(matchArray) ? matchArray[0] : '';
+    if (!!result) setContent({ ...content, testCaseTarget: testCaseTarget, [key]: e.target.value });
   };
 
   // * --------- 빈 칸인지 확인 ---------
@@ -65,13 +83,14 @@ function InputQuiz({ data }) {
               <Textarea isEmpty={isEmpty.testCase} value={content.testCase} placeholder="testCase를 입력하세요" maxLength="400" onChange={handleInputValue('testCase')} />
             </div>
             <div>
-              <Textarea isEmpty={isEmpty.testCaseTarget} value={content.testCaseTarget} placeholder="testCaseTarget을 입력하세요" maxLength="400" onChange={handleInputValue('testCaseTarget')} />
+              {/* //? 수정할 수 없는 Textarea. 디자인 처리할 때 다른 태그로 바꿔야될 것 같음 */}
+              <Textarea isEmpty={isEmpty.testCaseTarget} value={content.testCaseTarget} placeholder="testCaseTarget을 입력하세요" readonly maxLength="400" />
             </div>
           </div>
           <div>
             <h2>정답 / 해설</h2>
             <div>
-              <Input isEmpty={isEmpty.answer} type="text" value={content.answer} placeholder="정답을 쓰세요" maxLength="100" onChange={handleInputValue('answer')} />
+              <Input isEmpty={isEmpty.answer} type="text" value={content.answer} placeholder="정답을 쓰세요" size="50" maxLength="100" onChange={handleInputValue('answer')} />
             </div>
             <div>
               <Textarea isEmpty={isEmpty.explanation} type="text" value={content.explanation} placeholder="해설을 쓰세요" maxLength="400" onChange={handleInputValue('explanation')} />
