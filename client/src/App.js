b6e79d63cbe454c44d907012dbaf0bac76441ea0
EUNYUSEO <eyseo328@gmail.com>
2021-12-13 10:31:44 +0900

    Modify: CheatSheet 삭제

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import Navigation from './components/navigation';
 import Home from './pages/Home';
 import Tutorial from './pages/Tutorial';
 import QuizList from './pages/quiz/QuizList';
-import CheatSheet from './pages/CheatSheet';
 import MyInfo from './pages/myinfo';
 import ModifyMyInfo from './pages/myinfo/modify';
 import SetNewPassword from './pages/myinfo/SetNewPassword';
@@ -23,7 +22,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tutorial" element={<Tutorial />} />
-        <Route path="/cheatsheet" element={<CheatSheet />} />
         <Route path="/myinfo" element={<MyInfo />} />
         <Route path="/modify" element={<ModifyMyInfo />} />
         <Route path="/quizlist" element={<QuizList />} />
--- client/src/components/modal/index.js
+++ client/src/components/modal/index.js
@@ -39,7 +39,6 @@ const Modal = () => {
           </ModalContent>
         </ModalWrapper>
       </Background>
-      ;
     </>
   );
 };
