fcb6d9caa582d25f08dc0bf0637e132411fd8a49
EUNYUSEO <eyseo328@gmail.com>
2021-12-13 20:31:31 +0900

    Modify: button 클릭 시 효과 추가

--- client/src/components/user/SignInElements.js
+++ client/src/components/user/SignInElements.js
@@ -45,11 +45,18 @@ export const Input = styled.input.attrs({})`
 export const Button = styled.button`
   width: 50%;
   height: 40px;
-  font-size: 1.3rem;
+  font-size: 1.2rem;
   border-radius: 1rem;
   margin: 0.9rem 0;
   background-color: var(--color-blue);
   color: white;
+
+  transition: all 0.3s ease 0s;
+  letter-spacing: 2.5px;
+  :hover {
+    box-shadow: 0px 5px 8px rgba(171, 180, 221, 0.4);
+    transform: translateY(-2px);
+  }
 `;
 
 export const OauthBox = styled.div`
@@ -59,6 +66,11 @@ export const OauthBox = styled.div`
   .icon {
     width: 2.8rem;
     margin: 2rem 1.7rem 0.4rem 1.7rem;
+    transition: all 0.3s ease 0s;
+
+    :hover {
+      transform: translateY(-2px);
+    }
   }
 `;
 
@@ -71,5 +83,10 @@ export const Bottom = styled.div`
 `;
 
 export const Span = styled.span`
-  ${({ btn }) => (btn ? `cursor: pointer` : null)};
+  cursor: pointer;
+
+  :hover {
+    color: var(--color-blue);
+    text-decoration: underline;
+  }
 `;
--- client/src/components/user/SignUpElements.js
+++ client/src/components/user/SignUpElements.js
@@ -51,6 +51,13 @@ export const Button = styled.button`
   margin: 0.9rem 0;
   background-color: var(--color-blue);
   color: white;
+
+  transition: all 0.3s ease 0s;
+  letter-spacing: 2.5px;
+  :hover {
+    box-shadow: 0px 5px 8px rgba(171, 180, 221, 0.4);
+    transform: translateY(-2px);
+  }
 `;
 
 export const Bottom = styled.div`
@@ -62,5 +69,10 @@ export const Bottom = styled.div`
 `;
 
 export const Span = styled.span`
-  ${({ btn }) => (btn ? `cursor: pointer` : null)};
+  cursor: pointer;
+
+  :hover {
+    color: var(--color-blue);
+    text-decoration: underline;
+  }
 `;
