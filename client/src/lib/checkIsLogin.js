e96627a63d4bea34b231994e82214564f41055e7
EUNYUSEO <eyseo328@gmail.com>
2021-12-15 14:47:52 +0900

    Bugfix: 비밀번호 재입력 안하면 서버 요청 안함 #301

--- client/src/lib/checkIsLogin.js
+++ client/src/lib/checkIsLogin.js
@@ -6,17 +6,17 @@ export function checkIsLogin(res) {
   let nickname = '';
 
   // isLogin 헤더 확인
-  console.dir(res);
-  console.log('---성공---');
-  console.dir(res.response);
-  console.log('---실패---');
+  // console.dir(res);
+  // console.log('---성공---');
+  // console.dir(res.response);
+  // console.log('---실패---');
   if (!!res.response && !!res.response.headers) isLogin = res.response.headers.islogin;
   else isLogin = res.headers.islogin;
 
   // nickname 저장
   // res.data.nickname 또는 res.data.data.nickname;
-  if (!!res.data.nickname) {
-    console.log(res.data);
+  // console.log(res.data);
+  if (!!res.data && !!res.data.nickname) {
     nickname = res.data.nickname;
     if (!!res.data.data && !nickname) nickname = res.data.data.nickname;
   }
