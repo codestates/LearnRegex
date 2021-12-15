415751f7d6e3d770dfdcfa821a775d2b14398c3a
EUNYUSEO <eyseo328@gmail.com>
2021-12-15 14:32:36 +0900

    Bugfix: Navbar의 닉네임 유지 #298

--- client/src/lib/checkIsLogin.js
+++ client/src/lib/checkIsLogin.js
@@ -1,20 +1,28 @@
-import { setIsLogin } from '../modules/isLogin';
+import { setIsLogin, setNickname } from '../modules/isLogin';
 import { store } from '../index';
 
 export function checkIsLogin(res) {
   let isLogin = false;
   let nickname = '';
+
   // isLogin 헤더 확인
-  // console.dir(res);
+  console.dir(res);
+  console.log('---성공---');
+  console.dir(res.response);
+  console.log('---실패---');
   if (!!res.response && !!res.response.headers) isLogin = res.response.headers.islogin;
   else isLogin = res.headers.islogin;
+
   // nickname 저장
   // res.data.nickname 또는 res.data.data.nickname;
-  if (!!res.data) {
+  if (!!res.data.nickname) {
+    console.log(res.data);
     nickname = res.data.nickname;
     if (!!res.data.data && !nickname) nickname = res.data.data.nickname;
   }
-  store.dispatch(setIsLogin(isLogin === 'true', nickname));
+
+  if (nickname !== '') store.dispatch(setNickname(nickname));
+  store.dispatch(setIsLogin(isLogin === 'true'));
 }
 
 export function checkUserIsLogin() {
--- client/src/modules/isLogin.js
+++ client/src/modules/isLogin.js
@@ -1,6 +1,8 @@
 const SET_IS_LOGIN = 'SET_IS_LOGIN';
+const SET_NICKNAME = 'SET_NICKNAME';
 
-export const setIsLogin = (boolean, nickname = '') => ({ type: SET_IS_LOGIN, boolean, nickname });
+export const setIsLogin = (boolean) => ({ type: SET_IS_LOGIN, boolean });
+export const setNickname = (nickname = '') => ({ type: SET_NICKNAME, nickname });
 
 const initialState = { isLogin: false, nickname: '' };
 
@@ -8,7 +10,12 @@ export default function isLogin(state = initialState, action) {
   switch (action.type) {
     case SET_IS_LOGIN:
       return {
+        ...state,
         isLogin: action.boolean,
+      };
+    case SET_NICKNAME:
+      return {
+        ...state,
         nickname: action.nickname,
       };
     default:
