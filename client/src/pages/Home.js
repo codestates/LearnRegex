80b485862f42207b05ce80f034b112bcce6bb45d
yunjink <chloeeekim1109@gmail.com>
2021-12-16 23:15:37 +0900

    Modify: InfoData에 link 키 추가

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import HeroSection from '../components/heroSection';
 import InfoSection from '../components/infoSection';
-import { homeObjOne, homeObjTwo } from '../components/infoSection/Data';
+import { homeObjOne, homeObjTwo } from '../components/infoSection/InfoData';
 import Footer from '../components/footer/Footer';
 
 const Home = () => {
