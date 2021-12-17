40893b15cb71b1fca3111785e848877555791e5f
yunjink <chloeeekim1109@gmail.com>
2021-12-18 02:31:52 +0900

    Modify: Footer css 수정

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,34 +1,48 @@
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
-import { FooterContainer, FooterWrap, ContentsWrap, SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink } from './FooterElements';
+import { FooterContainer, FooterWrap, ContentsWrap, FooterInnerWrap, RepoWikiWrap, Logo, RepoWiki, WebsiteRights, GithubLinkWrap, GithubIcon, GithubLink } from './FooterElements';
 
 const Footer = () => {
   return (
     <FooterContainer>
       <FooterWrap>
         <ContentsWrap>
-          <SocialMediaWrap>
-            <SocialLogo to="/">LearnRegex</SocialLogo>
+          <FooterInnerWrap>
+            <RepoWikiWrap>
+              <Logo>LearnRegex&nbsp;</Logo>
+              <RepoWiki href="https://github.com/codestates/LearnRegex" target="_blank">
+                &nbsp;Repository&nbsp;
+              </RepoWiki>
+              <RepoWiki href="https://github.com/codestates/LearnRegex/wiki" target="_blank">
+                &nbsp;Wiki&nbsp;
+              </RepoWiki>
+            </RepoWikiWrap>
             <WebsiteRights>LearnRegex © 2021 All rights reserved.</WebsiteRights>
-            <SocialIcons>
-              <SocialIconLink href="/" target="_blank" aria-label="Github">
-                <FaGithub />
-                이승훈
-              </SocialIconLink>
-              <SocialIconLink href="/" target="_blank" aria-label="Github">
-                <FaGithub />
-                김연진
-              </SocialIconLink>
-              <SocialIconLink href="/" target="_blank" aria-label="Github">
-                <FaGithub />
-                서은유
-              </SocialIconLink>
-              <SocialIconLink href="/" target="_blank" aria-label="Github">
-                <FaGithub />
-                박진혁
-              </SocialIconLink>
-            </SocialIcons>
-          </SocialMediaWrap>
+            <GithubLinkWrap>
+              <GithubLink href="https://github.com/shleecloud" target="_blank" aria-label="Github">
+                <GithubIcon />
+                &nbsp;이승훈
+              </GithubLink>
+              <GithubLink href="https://github.com/yunjink" target="_blank" aria-label="Github">
+                <GithubIcon>
+                  <FaGithub />
+                </GithubIcon>
+                &nbsp;김연진
+              </GithubLink>
+              <GithubLink href="https://github.com/EUNYUSEO" target="_blank" aria-label="Github">
+                <GithubIcon>
+                  <FaGithub />
+                </GithubIcon>
+                &nbsp;서은유
+              </GithubLink>
+              <GithubLink href="https://github.com/hyucki" target="_blank" aria-label="Github">
+                <GithubIcon>
+                  <FaGithub />
+                </GithubIcon>
+                &nbsp;박진혁
+              </GithubLink>
+            </GithubLinkWrap>
+          </FooterInnerWrap>
         </ContentsWrap>
       </FooterWrap>
     </FooterContainer>
@@ -36,3 +50,5 @@ const Footer = () => {
 };
 
 export default Footer;
+
+//
